d311beafdfb15266822eeed50b4ac49774cab081
Ian Chen <czlianlap@gmail.com>
2020-08-23 20:48:15 -0500

    first commit

---
+++ js/exercise10.js
@@ -0,0 +1,40 @@
+$(function () {
+    $('.animLoading').show();
+    
+    var url = "http://www.randyconnolly.com/funwebdev/services/travel/cities.php";
+    var param = "iso=CA";
+    
+    // make request for list of cities for specified country
+        $.get(url, param)
+        .done(function (data) {
+            // loop through returned array of cities
+            $.each(data, function(index,city) {
+                // create new empty list item
+                var item = $('<li>');
+                
+                // add lat and long info from web service to each
+                // list item using HTML5 data- attributes 
+                item.attr( "data-lat", city.latitude);
+                item.attr( "data-long", city.longitude);
+                item.html('<a href="#">' + city.name + '</a>');
+                
+                // add list item to UL
+                $("#cities").append(item);
+            });
+        })
+        .fail(function (jqXHR) {
+            alert("Error: " + jqXHR.status);
+        })
+        .always(function () {
+            // all done so now hide the animated loading GIF
+            $('.animLoading').fadeOut("slow");
+        });
+
+    
+    // display map for selected city 
+    function displayMap(selectedCity) {
+
+    }
+
+
+});
